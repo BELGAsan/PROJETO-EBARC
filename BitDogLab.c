12e2fb4544238b98e0ac26607ac14f2c8bd0dd21
BELGAsan <sanatielevangelista8@gmail.com>
2025-03-23 20:43:24 -0300

    Update BitDogLab.c

--- BitDogLab.c
+++ BitDogLab.c
@@ -10,67 +10,65 @@
 #include "notes.h"
 #include "hardware/adc.h"
 
-// Declaração da função restart_system antes de ser usada
-void restart_system(uint8_t *buf, struct render_area *frame_area);
+// Declaração de funções
+void restart_system(uint8_t *buf, struct render_area *frame_area);  // Reinicia o sistema
+void play_alert_sound(uint pin);  // Toca um som de alerta no buzzer
 
-// Declaração da função play_alert_sound antes de sua utilização
-void play_alert_sound(uint pin);
-
-// Definição dos pinos
-const uint I2C_SDA_PIN = 14;  // Pino do barramento I2C (dados)
-const uint I2C_SCL_PIN = 15;  // Pino do barramento I2C (clock)
+// Definição dos pinos utilizados
+const uint I2C_SDA_PIN = 14;  // Pino SDA do I2C
+const uint I2C_SCL_PIN = 15;  // Pino SCL do I2C
 const uint BUZZER_A = 21;     // Pino para o buzzer A
-const uint BUZZER_B = 10;
+const uint BUZZER_B = 10;     // Pino para o buzzer B
 const uint BUTTON_A = 5;      // Pino para o botão A
 const uint BUTTON_B = 6;      // Pino para o botão B
-const uint LEDvr = 12;        // Pino para o LED 12 (agora será o LEDv)
+const uint LEDvr = 12;        // Pino para o LED 12 (LED de presença)
 const uint LEDa = 11;         // Pino para o LED 11 (LED do botão A)
-const uint LEDv = 13;         // Pino para o LED 13 (agora será o LEDvr)
+const uint LEDv = 13;         // Pino para o LED 13 (LED de confirmação)
 
-// Configurações do PWM
+// Configurações de PWM (modulação por largura de pulso)
 const float DIVISOR_CLK_PWM = 16.0;      
 const uint16_t PERIOD = 2000;            
 const uint16_t MAX_WRAP_DIV_BUZZER = 16; 
 const uint16_t MIN_WRAP_DIV_BUZZER = 2;  
 
-// Estados do botão
+// Estados do botão, para controle de debounce
 typedef enum {
     IDLE, DEBOUNCING_A, RELEASE_A, DEBOUNCING_B, RELEASE_B, ACTION_A, ACTION_B      
 } state_button;
 
 // Variáveis globais
-uint16_t wrap_div_buzzer = 8; 
-bool is_buzzer_a_playing = true; // Começa com o som tocando
+uint16_t wrap_div_buzzer = 8;  // Valor padrão de divisão do buzzer
+bool is_buzzer_a_playing = true; // Flag para controle do buzzer A
 
-// Função para tocar uma nota no buzzer
+// Função que toca uma nota no buzzer
 void play_note(uint pin, uint16_t wrap) {
-    int slice = pwm_gpio_to_slice_num(pin);
-    pwm_set_wrap(slice, wrap);
-    pwm_set_gpio_level(pin, wrap / wrap_div_buzzer);
-    pwm_set_enabled(slice, true);
+    int slice = pwm_gpio_to_slice_num(pin);  // Obtém o número do "slice" do pino
+    pwm_set_wrap(slice, wrap);  // Define o valor do wrap
+    pwm_set_gpio_level(pin, wrap / wrap_div_buzzer);  // Define o nível do pino
+    pwm_set_enabled(slice, true);  // Habilita o PWM
 }
 
 // Função para silenciar o buzzer
 void play_rest(uint pin) {
     int slice = pwm_gpio_to_slice_num(pin);
-    pwm_set_enabled(slice, false);
+    pwm_set_enabled(slice, false);  // Desativa o PWM (som)
 }
 
-// Função para desenhar texto na tela
+// Função que desenha texto no display
 void draw_text_display(const char *text[], uint8_t *buf, struct render_area *frame_area) {
     int y = 0;
     for (uint i = 0; i < 4; i++) { 
-        WriteString(buf, 5, y, (char *)text[i]);
-        y += 8;
+        WriteString(buf, 5, y, (char *)text[i]);  // Escreve o texto no buffer
+        y += 8;  // Incrementa o Y para o próximo texto
     }
-    render(buf, frame_area);
+    render(buf, frame_area);  // Renderiza o conteúdo do buffer
 }
 
-// Função para ler os botões com debounce
+// Função que lê o estado dos botões com debounce
 void read_buttons(uint8_t *buf, struct render_area *frame_area) {
     static state_button s = IDLE;    
-    static uint cnt = 0;              // contador para o debounce
-    const uint DEBOUNCE_CYCLES = 50;  // tempo de debounce
+    static uint cnt = 0;              // Contador para o debounce
+    const uint DEBOUNCE_CYCLES = 50;  // Tempo de debounce
 
     const char *textBTN_A[] = {
         "   PRESENCA    ",
@@ -87,96 +85,85 @@ void read_buttons(uint8_t *buf, struct render_area *frame_area) {
     };
 
     switch (s) {
-        case IDLE:
+        case IDLE:  // Estado inicial
             if (gpio_get(BUTTON_A) == 0) {  
-                draw_text_display(textBTN_A, buf, frame_area);
-                s = DEBOUNCING_A;
+                draw_text_display(textBTN_A, buf, frame_area);  // Exibe mensagem do botão A
+                s = DEBOUNCING_A;  // Muda para estado de debounce do botão A
             }          
             if (gpio_get(BUTTON_B) == 0) { 
-                draw_text_display(textBTN_B, buf, frame_area);
-                s = DEBOUNCING_B;
+                draw_text_display(textBTN_B, buf, frame_area);  // Exibe mensagem do botão B
+                s = DEBOUNCING_B;  // Muda para estado de debounce do botão B
             }          
-            cnt = 0;
+            cnt = 0;  // Reseta o contador de debounce
             break;
-        case DEBOUNCING_A:
+        case DEBOUNCING_A:  // Espera o botão A ser liberado
             if (gpio_get(BUTTON_A) == 0) {
-                cnt++;
+                cnt++;  // Conta o tempo de debounce
                 if (cnt > DEBOUNCE_CYCLES) {
                     cnt = 0;
-                    s = RELEASE_A;
+                    s = RELEASE_A;  // Quando o botão A for pressionado, muda para RELEASE_A
                 }
             } else {
-                s = IDLE;
+                s = IDLE;  // Se o botão A foi solto, volta para o estado IDLE
             }
             break;
-        case DEBOUNCING_B:
+        case DEBOUNCING_B:  // Espera o botão B ser liberado
             if (gpio_get(BUTTON_B) == 0) {
-                cnt++;
+                cnt++;  // Conta o tempo de debounce
                 if (cnt > DEBOUNCE_CYCLES) {
                     cnt = 0;
-                    s = RELEASE_B;
+                    s = RELEASE_B;  // Quando o botão B for pressionado, muda para RELEASE_B
                 }
             } else {
-                s = IDLE;
+                s = IDLE;  // Se o botão B foi solto, volta para o estado IDLE
             }
             break;
-        case RELEASE_A:
+        case RELEASE_A:  // Quando o botão A for solto
             if (gpio_get(BUTTON_A) == 1)
-                s = ACTION_A;
+                s = ACTION_A;  // Muda para ação do botão A
             break;
-        case RELEASE_B:
+        case RELEASE_B:  // Quando o botão B for solto
             if (gpio_get(BUTTON_B) == 1)
-                s = ACTION_B;
+                s = ACTION_B;  // Muda para ação do botão B
             break;
-        case ACTION_A:
+        case ACTION_A:  // Ação associada ao botão A
             s = IDLE;
-            gpio_put(LEDvr, 0);  // Desliga LED 12 (LEDvr)
-            gpio_put(LEDv, 0);    // Desliga LED 13 (LEDv)
-            gpio_put(LEDa, 1);   // Acende LED 11 (LEDa)
-            play_rest(BUZZER_A); // Desliga o som (A) quando o botão A é pressionado
-            is_buzzer_a_playing = false; 
+            gpio_put(LEDvr, 0);  // Desliga o LED 12
+            gpio_put(LEDv, 0);    // Desliga o LED 13
+            gpio_put(LEDa, 1);   // Acende o LED 11
+            play_rest(BUZZER_A); // Desliga o buzzer A
+            is_buzzer_a_playing = false;  // Desliga o buzzer A
             break;
-        case ACTION_B:
+        case ACTION_B:  // Ação associada ao botão B
             s = IDLE;
-            gpio_put(LEDa, 0);   // Desliga LED 11 (LEDa)
-            gpio_put(LEDvr, 1);  // Acende LED 12 (LEDvr)
+            gpio_put(LEDa, 0);   // Desliga o LED 11
+            gpio_put(LEDvr, 1);  // Acende o LED 12
             play_rest(BUZZER_A);  
             is_buzzer_a_playing = false; 
 
-            // Espera 5 segundos antes de reiniciar o sistema
-            sleep_ms(5000);
-            
-            // Reinicia o sistema, como se tivesse acabado de ser ligado
-            gpio_put(LEDvr, 0);  // Desliga LED 12 (LEDvr)
-            gpio_put(LEDa, 0);   // Desliga LED 11 (LEDa)
+            sleep_ms(5000);  // Espera 5 segundos antes de reiniciar o sistema
 
-            // Reinicializa a tela e os componentes
-            restart_system(buf, frame_area);  // Chama a função que reinicializa a tela corretamente
+            restart_system(buf, frame_area);  // Chama a função que reinicializa o sistema
 
-            // Reativar o som quando o sistema reiniciar
-            play_alert_sound(BUZZER_A);  // Reinicia o som
+            play_alert_sound(BUZZER_A);  // Toca novamente o alerta
             is_buzzer_a_playing = true;
 
-            // **Aqui está a modificação**: Acende o LED azul (LEDvr) após o reinício
-            gpio_put(LEDvr, 1);  // Acende o LED 12 (LEDvr)
-
+            gpio_put(LEDvr, 1);  // Acende o LED 12 após reiniciar
             break;
         default:
-            s = IDLE;
-            cnt = 0;
+            s = IDLE;  // Reseta o estado para IDLE
+            cnt = 0;  // Reseta o contador de debounce
     }
 }
 
 // Função para reiniciar o sistema e renderizar o display corretamente
 void restart_system(uint8_t *buf, struct render_area *frame_area) {
-    // Reinicializa a tela
-    SSD1306_init();
+    SSD1306_init();  // Inicializa o display SSD1306
 
-    // Limpa o buffer e renderiza
-    memset(buf, 0, SSD1306_BUF_LEN);
-    render(buf, frame_area);
+    memset(buf, 0, SSD1306_BUF_LEN);  // Limpa o buffer
+    render(buf, frame_area);  // Renderiza o conteúdo do buffer
 
-    // Exibe a nova mensagem após reinício
+    // Exibe a mensagem de reinício
     const char *restart_text[] = {
         "  OBRIGADO ATÉ  ",
         "  O PROXIMO     ",
@@ -185,25 +172,24 @@ void restart_system(uint8_t *buf, struct render_area *frame_area) {
     };
     draw_text_display(restart_text, buf, frame_area);
 
-    // Aguarda 5 segundos antes de iniciar o sistema novamente
-    sleep_ms(5000);
+    sleep_ms(5000);  // Aguarda 5 segundos antes de reiniciar o sistema
 }
 
-// Função para tocar o alerta inicial (3 notas)
+// Função para tocar o alerta sonoro (3 notas)
 void play_alert_sound(uint pin) {
-    uint melody_alert[] = {NOTE_C4, NOTE_E4, NOTE_G4};  // 3 notas de alerta
+    uint melody_alert[] = {NOTE_C4, NOTE_E4, NOTE_G4};  // Melodia de 3 notas
     uint durations_alert[] = {500, 500, 500};           // Duração de 500 ms para cada nota
-    int repeat_count = 0;  // Contador para as repetições
+    int repeat_count = 0;  // Contador de repetições
 
     while (repeat_count < 3) {  // Repetir a melodia 3 vezes
         for (int i = 0; i < 3; i++) {
-            play_note(pin, melody_alert[i]);
-            sleep_ms(durations_alert[i]);  // Espera a duração da nota
+            play_note(pin, melody_alert[i]);  // Toca cada nota
+            sleep_ms(durations_alert[i]);  // Espera a duração de cada nota
         }
-        repeat_count++;  // Incrementa a contagem de repetições
-        sleep_ms(500);  // Pausa entre os ciclos de melodia
+        repeat_count++;  // Incrementa o contador de repetições
+        sleep_ms(500);  // Pausa entre as repetições
     }
-    play_rest(pin);  // Quando terminar de tocar as 3 vezes, a melodia para
+    play_rest(pin);  // Desliga o buzzer
 }
 
 // Função para configurar o áudio e os pinos
@@ -225,24 +211,22 @@ void setup_audio() {
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_set_dir(BUTTON_B, GPIO_IN);
 
-    gpio_pull_up(BUTTON_A);
-    gpio_pull_up(BUTTON_B);
+    gpio_pull_up(BUTTON_A);  // Ativa o pull-up interno no botão A
+    gpio_pull_up(BUTTON_B);  // Ativa o pull-up interno no botão B
 
-    // Acende o LED 13 (LEDv) ao iniciar
-    gpio_put(LEDv, 1);  // LED 13 aceso ao iniciar
-    gpio_put(LEDa, 0);
+    gpio_put(LEDv, 1);  // Acende o LED 13 (LEDv) ao iniciar
+    gpio_put(LEDa, 0);  // Desliga o LED 11
 
-    slice = pwm_gpio_to_slice_num(BUZZER_A);
-    pwm_set_clkdiv(slice, DIVISOR_CLK_PWM);
-    pwm_set_wrap(slice, PERIOD);
-    pwm_set_enabled(slice, true);
-    
-    // Começa a tocar o som automaticamente ao ligar
-    play_alert_sound(BUZZER_A);  // Alerta sonoro
+    slice = pwm_gpio_to_slice_num(BUZZER_A);  // Obtém o slice do buzzer
+    pwm_set_clkdiv(slice, DIVISOR_CLK_PWM);  // Define o divisor de clock
+    pwm_set_wrap(slice, PERIOD);  // Define o valor do wrap do PWM
+    pwm_set_enabled(slice, true);  // Habilita o PWM
+
+    play_alert_sound(BUZZER_A);  // Toca o som de alerta ao ligar
     is_buzzer_a_playing = true;
 }
 
-// Função para ler os valores do joystick
+// Função para ler os valores do joystick (não está sendo usada no código atual)
 bool read_joystick() {
     uint joystick_x, joystick_y;
 
@@ -260,15 +244,16 @@ bool read_joystick() {
 
     // Se o joystick for movido (valores diferentes do centro)
     if (joystick_x < 2000 || joystick_x > 4000 || joystick_y < 2000 || joystick_y > 4000) {
-        return true;
+        return true;  // Retorna true se o joystick for movido
     }
-    return false;
+    return false;  // Caso contrário, retorna false
 }
 
 // Função principal
 int main() {
-    stdio_init_all();
-    setup_audio();
+    stdio_init_all();  // Inicializa o sistema de entrada e saída
+
+    setup_audio();  // Configura o áudio e os pinos
 
     // Configura o display SSD1306
     i2c_init(i2c1, SSD1306_I2C_CLK * 1000);
@@ -302,17 +287,14 @@ int main() {
     // Começa a interação com os botões e joystick
     while (true) {
         if (read_joystick()) {
-            gpio_put(LEDvr, 1); // Acende LED 12 (LEDvr)
+            gpio_put(LEDvr, 1); // Acende LED 12 (LEDvr) quando o joystick é movido
             while (!gpio_get(BUTTON_A)) {
-                // Aguarda o botão A ser pressionado
-                sleep_ms(100);
+                sleep_ms(100);  // Aguarda o botão A ser pressionado
             }
-            gpio_put(LEDvr, 0);  // Desliga LED 12
-            // Continua o código após pressionar o botão A
-            read_buttons(buf, &frame_area);
-        }
-        else {
-            read_buttons(buf, &frame_area);  // Normal interação com botões
+            gpio_put(LEDvr, 0);  // Desliga LED 12 após pressionar o botão A
+            read_buttons(buf, &frame_area);  // Continua a leitura dos botões
+        } else {
+            read_buttons(buf, &frame_area);  // Caso o joystick não seja movido, só interage com os botões
         }
     }
 
